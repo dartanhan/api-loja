66add1ca426120e88cad7d7436b6536e9a213bc1
Dartanhan <dartanhan.lima@gmail.com>
2024-03-20 17:27:52 -0300

    add requet ajax controller

--- app/Http/Controllers/AuditsController.php
+++ app/Http/Controllers/AuditsController.php
@@ -52,26 +52,24 @@ class AuditsController extends Controller
 
     public function datatableAuditUpdate(Request $request){
 
-      $auditsUpdate = Audit::
-                  leftJoin('loja_produtos_variacao as va', 'audits.auditable_id','=', 'va.id' )
-                  ->leftJoin('loja_produtos_new as pn', 'va.products_id','=', 'pn.id' ) 
-                  ->leftJoin('users', 'audits.user_id','=', 'users.id' )
-                  ->select(
-                    'users.name as usuario',
-                    'audits.event as evento',
-                    DB::raw("CONCAT(pn.descricao, ' - ', va.variacao) AS variacao"),
-                    'audits.old_values',
-                    'audits.new_values',
-                    'audits.updated_at')
-                 ->where('audits.auditable_type', 'App\Http\Models\ProdutoVariation')
-                 ->where('audits.event', 'updated');
+      if($request->ajax()){
+          $auditsUpdate = Audit::
+            leftJoin('loja_produtos_variacao as va', 'audits.auditable_id','=', 'va.id' )
+            ->leftJoin('loja_produtos_new as pn', 'va.products_id','=', 'pn.id' ) 
+            ->leftJoin('users', 'audits.user_id','=', 'users.id' )
+            ->select(
+              'users.name as usuario',
+              'audits.event as evento',
+              DB::raw("CONCAT(pn.descricao, ' - ', va.variacao) AS variacao"),
+              'audits.old_values',
+              'audits.new_values',
+              'audits.updated_at')
+          ->where('audits.auditable_type', 'App\Http\Models\ProdutoVariation')
+          ->where('audits.event', 'updated');
 
-                // return  DataTables::of($auditsUpdate)->make(true);
-                // return DataTables::of($auditsUpdate)
-                // ->setFilteredRecords(100)
-                // ->toJson();
-
-                return DataTables::of($auditsUpdate)->toJson();
+        return DataTables::of($auditsUpdate)->toJson();
+      }
+      
                  
     }
 }
