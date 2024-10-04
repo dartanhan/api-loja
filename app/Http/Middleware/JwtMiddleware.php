43abb003467d27145ca20df94e292736e1e6aaa5
Dartanhan <dartanhan.silva@extreme.digital>
2024-10-04 17:20:47 -0300

    add, opção de criar carrinho pela API

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Exception;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 
 class JwtMiddleware extends BaseMiddleware
 {
@@ -23,10 +25,10 @@ class JwtMiddleware extends BaseMiddleware
             $user = JWTAuth::authenticate($token);
             //$user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
-            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+            if ($e instanceof TokenInvalidException){
                 return response()->json(['status' => 'Token é inválido'],404,[],JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'O token está expirado'], 500,[],JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+            }else if ($e instanceof TokenExpiredException){
+                return response()->json(['status' => 'O token está expirado'], 401,[],JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
             }else{
                 return response()->json(['status' => 'Token de autorização não encontrado'],404,[],JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
             }
