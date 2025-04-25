88d42358d2842fd6a50f290a59c5e7d888eade4f
Dartanhan <dartanhan.silva@extreme.digital>
2025-04-25 15:51:28 -0300

    ajuste CashBackController.php e model com propriedades

--- app/Http/Models/VendasCashBack.php
+++ app/Http/Models/VendasCashBack.php
@@ -1,15 +1,33 @@
 <?php
 namespace App\Http\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class VendasCashBack
+ *
+ * @property int $id
+ * @property int $cliente_id
+ * @property float $valor
+ * @property int $status
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class VendasCashBack extends Model
 {
     public $table = 'loja_vendas_cashback';
     protected $fillable = ['cliente_id','venda_id','valor','created_at','update_at'];
 
      // Define o relacionamento belongsTo
-     public function vendas()
+
+
+    public static function cashback(string $string, int $id)
+    {
+        return VendasCashBack::query()->where($string, $id)->where( 'status', 0)->sum('valor');
+    }
+
+    public function vendas()
      {
          return $this->belongsTo(Vendas::class, 'venda_id');
      }
