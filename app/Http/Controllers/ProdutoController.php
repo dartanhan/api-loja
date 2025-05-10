cf60f02d983c44a18e5c659219517f481fa30ad2
Dartanhan <dartanhan.silva@extreme.digital>
2025-05-10 11:23:13 -0300

    ajuste exibir produtos mesmo sem fotos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -27,11 +27,12 @@ use Yajra\DataTables\Facades\DataTables;
 
 class ProdutoController extends Controller
 {
-    protected $request,$produto,$fornecedor,$category,$cor,$produtoImage,$produtoVariation,$produtoControle,$origem_nfce;
+    protected $request, $produto, $fornecedor, $category, $cor, $produtoImage, $produtoVariation, $produtoControle, $origem_nfce;
 
-    public function __construct(Request $request, Produto $produto,Fornecedor $fornecedor, Categoria $category,
+    public function __construct(Request $request, Produto $produto, Fornecedor $fornecedor, Categoria $category,
                                 Cor $cor, ProdutoImagem $produto_image, ProdutoVariation $produtoVariation,
-                                ProdutoControle $produtoControle, OrigemNfce  $origem_nfce ){
+                                ProdutoControle $produtoControle, OrigemNfce $origem_nfce)
+    {
         $this->request = $request;
         $this->produto = $produto;
         $this->fornecedor = $fornecedor;
@@ -40,7 +41,7 @@ class ProdutoController extends Controller
         $this->produtoImage = $produto_image;
         $this->produtoVariation = $produtoVariation;
         $this->produtoControle = $produtoControle;
-        $this->origem_nfce =  $origem_nfce;
+        $this->origem_nfce = $origem_nfce;
     }
 
     /**
@@ -50,15 +51,15 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        if(Auth::check() === true){
-            $suppliers = $this->fornecedor->where('status',true)->orderBy('nome', 'ASC')->get();
-            $categories = $this->category->where('status',true)->orderBy('nome', 'ASC')->get();
-            $cores = $this->cor->where('status',true)->orderBy('nome', 'ASC')->get();
+        if (Auth::check() === true) {
+            $suppliers = $this->fornecedor->where('status', true)->orderBy('nome', 'ASC')->get();
+            $categories = $this->category->where('status', true)->orderBy('nome', 'ASC')->get();
+            $cores = $this->cor->where('status', true)->orderBy('nome', 'ASC')->get();
             $origem_nfces = $this->origem_nfce->orderBy('codigo', 'ASC')->get();
 
-            $user_data = Usuario::where("user_id",auth()->user()->id)->first();
+            $user_data = Usuario::where("user_id", auth()->user()->id)->first();
 
-            return view('admin.produto', compact('origem_nfces','categories','cores','user_data'));
+            return view('admin.produto', compact('origem_nfces', 'categories', 'cores', 'user_data'));
         }
 
         return redirect()->route('admin.login');
@@ -75,9 +76,9 @@ class ProdutoController extends Controller
         try {
 
             //$ret =  $this->produto::with('products')
-            $query =  $this->produto::with('produtoImagens')
-                ->leftJoin('loja_fornecedores','lpn.fornecedor_id','=' ,'loja_fornecedores.id')
-                ->leftJoin('loja_categorias','lpn.categoria_id','=' ,'loja_categorias.id')
+            $query = $this->produto::with('produtoImagens')
+                ->leftJoin('loja_fornecedores', 'lpn.fornecedor_id', '=', 'loja_fornecedores.id')
+                ->leftJoin('loja_categorias', 'lpn.categoria_id', '=', 'loja_categorias.id')
                 ->select(
                     'lpn.id',
                     'lpn.codigo_produto',
@@ -88,14 +89,14 @@ class ProdutoController extends Controller
                     (DB::raw("DATE_FORMAT(lpn.updated_at, '%d/%m/%Y %H:%i:%s') as updated"))
 
                 )->from('loja_produtos_new as lpn')
-                ->where('lpn.status',1) //somente ativos
+                ->where('lpn.status', 1) //somente ativos
                 ->groupBy('lpn.id')
                 ->orderBy('lpn.id', 'DESC');
 
-            if(!empty($query)) {
+            if (!empty($query)) {
                 return DataTables::of($query)->make(true);
-            }  else {
-                return Response()->json(array('data'=>''));
+            } else {
+                return Response()->json(array('data' => ''));
             }
 
         } catch (Throwable $e) {
@@ -110,51 +111,50 @@ class ProdutoController extends Controller
      */
     public function store()
     {
-        try{
-             // dd($this->request->all());
+        try {
+            // dd($this->request->all());
             //   dd(count($this->request->allFiles()['images0']));
             $produto_id = $this->request->input("produto_id");
-            if($produto_id == null){
+            if ($produto_id == null) {
                 $msg = "Produto Cadastrado com sucesso!";
                 $rules = [
-                            'codigo_produto' => 'required|unique:'.$this->produto->table.'|max:15',
-                            'descricao' => 'required|max:155',
-                            'origem' => 'required|max:5',
-                            'cest' => 'required|max:15',
-                            'ncm' => 'required|max:15',
-                            'categoria_id' => 'required|max:5',
-                    ];
+                    'codigo_produto' => 'required|unique:' . $this->produto->table . '|max:15',
+                    'descricao' => 'required|max:155',
+                    'origem' => 'required|max:5',
+                    'cest' => 'required|max:15',
+                    'ncm' => 'required|max:15',
+                    'categoria_id' => 'required|max:5',
+                ];
 
-            }else{
+            } else {
                 $msg = "Produto Atualizado com sucesso!";
                 $rules = [
-                            'codigo_produto' => 'required|max:15|unique:'.$this->produto->table.',codigo_produto,'. $produto_id,
-                            'descricao' => 'required|max:155',
-                            'origem' => 'required|max:5',
-                            'cest' => 'required|max:15',
-                            'ncm' => 'required|max:15',
-                            'categoria_id' => 'required|max:5'
-                        ];
+                    'codigo_produto' => 'required|max:15|unique:' . $this->produto->table . ',codigo_produto,' . $produto_id,
+                    'descricao' => 'required|max:155',
+                    'origem' => 'required|max:5',
+                    'cest' => 'required|max:15',
+                    'ncm' => 'required|max:15',
+                    'categoria_id' => 'required|max:5'
+                ];
             }
 
             //Valida o form
-            $validated = Validator::make($this->request->all(),$rules,$messages = [
-                'codigo_produto.required'=> 'Código do produto é obrigatório!',
-                'codigo_produto.unique'  => 'Código do produto já cadastrado!',
-                'codigo_produto.max'=> 'Código do produto deve ser menos que 15 caracteres!',
-                'descricao.required'=> 'Descrição do produto é obrigatório!',
-                'descricao.max'=> 'Descrição limtado a 155 caracteres!',
-                'origem.required'=> 'A origem é obrigatório!',
-                'cest.required'=> 'O cest é obrigatório!',
-                'ncm.required'=> 'O ncm é obrigatório!',
-                'categoria_id.required'=> 'A Categoria é obrigatória!',
+            $validated = Validator::make($this->request->all(), $rules, $messages = [
+                'codigo_produto.required' => 'Código do produto é obrigatório!',
+                'codigo_produto.unique' => 'Código do produto já cadastrado!',
+                'codigo_produto.max' => 'Código do produto deve ser menos que 15 caracteres!',
+                'descricao.required' => 'Descrição do produto é obrigatório!',
+                'descricao.max' => 'Descrição limtado a 155 caracteres!',
+                'origem.required' => 'A origem é obrigatório!',
+                'cest.required' => 'O cest é obrigatório!',
+                'ncm.required' => 'O ncm é obrigatório!',
+                'categoria_id.required' => 'A Categoria é obrigatória!',
             ]);
 
             //Verifica se temos erros no form
-            if ($validated->fails())
-            {
+            if ($validated->fails()) {
                 $error = $validated->errors()->first();
-                return Response::json(array('success' => false,'message' => $error), 400);
+                return Response::json(array('success' => false, 'message' => $error), 400);
             }
 
             $data["codigo_produto"] = $this->request->input("codigo_produto");
@@ -172,7 +172,7 @@ class ProdutoController extends Controller
             $matchThese = array('id' => $produto_id);
             $products = $this->produto::updateOrCreate($matchThese, $data);
 
-           // dd($products);
+            // dd($products);
             // echo $produtos->id;
             /**
              * Quantidade de variações
@@ -189,23 +189,23 @@ class ProdutoController extends Controller
             /**
              *   Formata em decimal par o banco
              */
-            $formatter = new NumberFormatter('pt_BR',  NumberFormatter::DECIMAL);
+            $formatter = new NumberFormatter('pt_BR', NumberFormatter::DECIMAL);
 
-            for ($i=0; $i<$qtd_lines; $i++) {
+            for ($i = 0; $i < $qtd_lines; $i++) {
                 $dateString = $this->request->input("validade")[$i];
 
-                 if ($dateString === "00/00/0000") {
-                     $formattedDate = "0000-00-00";
-                }else{
-                    $formattedDate = Carbon::createFromFormat('d/m/Y',$dateString)->format('Y-m-d');
+                if ($dateString === "00/00/0000") {
+                    $formattedDate = "0000-00-00";
+                } else {
+                    $formattedDate = Carbon::createFromFormat('d/m/Y', $dateString)->format('Y-m-d');
                 }
 
-                $data["subcodigo"] = $data["codigo_produto"].$this->request->input("subcodigo")[$i];
+                $data["subcodigo"] = $data["codigo_produto"] . $this->request->input("subcodigo")[$i];
                 $data["variacao"] = $this->request->input("variacao")[$i];
-                $data["valor_varejo"] = $formatter->parse(str_replace(['R$', ' '], '',$this->request->input("valor_varejo")[$i]));
-                $data["valor_atacado"] = $formatter->parse(str_replace(['R$', ' '], '',$this->request->input("valor_atacado_10un")[$i]));
-                $data["valor_atacado_10un"] = $formatter->parse(str_replace(['R$', ' '], '',$this->request->input("valor_atacado_10un")[$i]));
-                $data["valor_produto"] = $formatter->parse(str_replace(['R$', ' '], '',$this->request->input("valor_produto")[$i]));
+                $data["valor_varejo"] = $formatter->parse(str_replace(['R$', ' '], '', $this->request->input("valor_varejo")[$i]));
+                $data["valor_atacado"] = $formatter->parse(str_replace(['R$', ' '], '', $this->request->input("valor_atacado_10un")[$i]));
+                $data["valor_atacado_10un"] = $formatter->parse(str_replace(['R$', ' '], '', $this->request->input("valor_atacado_10un")[$i]));
+                $data["valor_produto"] = $formatter->parse(str_replace(['R$', ' '], '', $this->request->input("valor_produto")[$i]));
                 $data["quantidade"] = $this->request->input("quantidade")[$i];
                 $data["quantidade_minima"] = $this->request->input("quantidade_minima")[$i];
                 //se o status do Pai for INATIVO (0), seta 0 para os filhos
@@ -232,15 +232,15 @@ class ProdutoController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return JsonResponse
      */
     public function show($id)
     {
         $data = $this->produto::select('codigo_produto')->orderBy("id", "desc")->first();
-        $id = $this->produto::max('id')+1; //pega próximo ID para criar  a variação do produto
+        $id = $this->produto::max('id') + 1; //pega próximo ID para criar  a variação do produto
 
-        if($data == null)
+        if ($data == null)
             return Response::json(array('success' => true, "data" => 1000, "id" => "01"), 200);
 
         return Response::json(array('success' => true, "data" => $data->codigo_produto + 1, "id" => "01"), 200);
@@ -267,7 +267,7 @@ class ProdutoController extends Controller
     public function update($id)
     {
         try {
-            $produto =  $this->produto->find($id);
+            $produto = $this->produto->find($id);
             $produto->status = 1;
             $produto->update(); // Salve as alterações no banco de dados
 
@@ -295,10 +295,11 @@ class ProdutoController extends Controller
      * @param int $id
      * @return JsonResponse
      */
-    public function pictures(int $id){
+    public function pictures(int $id)
+    {
 
         try {
-           $data = $this->produtoImage::select('id','produto_variacao_id','path')->where('produto_variacao_id',$id)->get();
+            $data = $this->produtoImage::select('id', 'produto_variacao_id', 'path')->where('produto_variacao_id', $id)->get();
 
         } catch (Throwable $e) {
             return Response::json(['error' => $e->getMessage()], 500);
@@ -310,12 +311,13 @@ class ProdutoController extends Controller
      *
      * @param int $id
      */
-    public function getProducts(int $id){
+    public function getProducts(int $id)
+    {
         try {
 
-            $ret =  $this->produto::with('products')
-                ->select("id","codigo_produto","descricao","status","fornecedor_id","categoria_id","ncm",'cest',"origem_id")
-                ->where('id',$id)->first();
+            $ret = $this->produto::with('products')
+                ->select("id", "codigo_produto", "descricao", "status", "fornecedor_id", "categoria_id", "ncm", 'cest', "origem_id")
+                ->where('id', $id)->first();
 
 
         } catch (Throwable $e) {
@@ -325,18 +327,17 @@ class ProdutoController extends Controller
     }
 
 
-
     public function indexNew()
     {
-        if(Auth::check() === true){
-            $suppliers = $this->fornecedor->where('status',true)->orderBy('nome', 'ASC')->get();
-            $categories = $this->category->where('status',true)->orderBy('nome', 'ASC')->get();
-            $cores = $this->cor->where('status',true)->orderBy('nome', 'ASC')->get();
+        if (Auth::check() === true) {
+            $suppliers = $this->fornecedor->where('status', true)->orderBy('nome', 'ASC')->get();
+            $categories = $this->category->where('status', true)->orderBy('nome', 'ASC')->get();
+            $cores = $this->cor->where('status', true)->orderBy('nome', 'ASC')->get();
             $origem_nfces = $this->origem_nfce->orderBy('codigo', 'ASC')->get();
 
-            $user_data = Usuario::where("user_id",auth()->user()->id)->first();
+            $user_data = Usuario::where("user_id", auth()->user()->id)->first();
 
-            return view('admin.produto-new', compact('origem_nfces','categories','cores','user_data'));
+            return view('admin.produto-new', compact('origem_nfces', 'categories', 'cores', 'user_data'));
         }
 
         return redirect()->route('admin.login');
@@ -344,7 +345,7 @@ class ProdutoController extends Controller
     }
 
 
-    public function fornecedoresProdutosBaixoEstoque ()
+    public function fornecedoresProdutosBaixoEstoque()
     {
 //        $inicioPeriodo = now()->subDays(30);
 //        $fimPeriodo = now();
@@ -369,16 +370,13 @@ class ProdutoController extends Controller
 
     public function getProdutosEstourados(Request $request)
     {
-       // $inicio = now()->subDays(30);
-       // $fim = Carbon::now();
-        $inicio = $request->input('data_inicio',  now()->startOfMonth());
+        $inicio = $request->input('data_inicio', now()->startOfMonth());
         $fim = $request->input('data_fim', now()->endOfMonth());
 
         $dados = DB::table('loja_vendas_produtos as vp')
-          // ->join('loja_vendas as v', 'vp.venda_id', '=', 'v.id')
             ->join('loja_produtos_variacao as pv', 'vp.codigo_produto', '=', 'pv.subcodigo')
             ->join('loja_fornecedores as f', 'pv.fornecedor', '=', 'f.id')
-            ->join('loja_produtos_imagens as i', 'pv.id', '=', 'i.produto_variacao_id')
+            ->leftJoin('loja_produtos_imagens as i', 'pv.id', '=', 'i.produto_variacao_id')
             ->select(
                 'f.nome as fornecedor',
                 'vp.codigo_produto',
@@ -388,7 +386,7 @@ class ProdutoController extends Controller
                 'i.path as imagem'
             )
             ->whereBetween('vp.created_at', [$inicio, $fim])
-            ->groupBy('vp.codigo_produto', 'vp.descricao', 'pv.quantidade', 'f.nome')
+            ->groupBy('vp.codigo_produto', 'vp.descricao', 'pv.quantidade', 'f.nome', 'i.path')
             ->havingRaw('pv.quantidade < SUM(vp.quantidade)')
             ->get()
             ->groupBy('fornecedor');
@@ -402,5 +400,4 @@ class ProdutoController extends Controller
 
         return response()->json(['data' => $resultado]);
     }
-
 }
